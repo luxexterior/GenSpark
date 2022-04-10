b0de91c0e55243d95b322a1d104c5db77ba70c18
Brian <brianglenn@gmail.com>
2022-04-10 14:41:32 -0700

    adding guess the number project

---
+++ Guess the Number/src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        boolean playAgain = true;
+
+        System.out.println("Hello, what is your name?");
+        String userName = input.nextLine();
+        System.out.println("Well, " + userName + ", I am thinking of a number between 1 and 20.");
+
+        while (playAgain == true) {
+           int attempt = 0;
+           int guess = 0;
+           int correctAnswer = ((int) (Math.random() * 20) + 1);
+
+           while(guess != correctAnswer && attempt < 6) {
+               System.out.println("Take a guess");
+               guess = Integer.parseInt(input.nextLine());
+               attempt++;
+
+               if(guess < correctAnswer) {
+                   System.out.println("Your guess is too low.");
+               } else if (guess > correctAnswer) {
+                   System.out.println("Your guess is too high.");
+               } else if (guess == correctAnswer) {
+                   System.out.println("Good job, " + userName + "! You guessed my number in " + attempt + " guesses!");
+               } else {
+                   System.out.println("Invalid input. Try again.");
+               }
+               if (attempt >= 6) {
+                   System.out.println("Too many guesses. Better luck next time.");
+               }
+           }
+            System.out.println("Would you like to play again? (y/n)");
+           playAgain = input.nextLine().equals("y");
+        }
+
+
+
+
+
+
+
+    }
+}
